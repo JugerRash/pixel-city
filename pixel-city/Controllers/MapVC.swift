966835d88cb2f6ec036447679d634d3fff3a05f0
juger <juger@jugers-Mac.local>
2019-07-15 01:57:22 -0700

    first lesson

---
+++ pixel-city/Controllers/MapVC.swift
@@ -0,0 +1,20 @@
+//
+//  MapVC.swift
+//  pixel-city
+//
+//  Created by juger on 7/15/19.
+//  Copyright © 2019 juger. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
