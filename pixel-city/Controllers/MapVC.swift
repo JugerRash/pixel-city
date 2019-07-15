d823095c7f32687d4b5bd916623082d9e76e980a
juger <juger@jugers-Mac.local>
2019-07-15 07:45:14 -0700

    getting user location

--- pixel-city/Controllers/MapVC.swift
+++ pixel-city/Controllers/MapVC.swift
@@ -7,14 +7,58 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class MapVC: UIViewController {
 
+    // Outlets :
+    @IBOutlet weak var mapView: MKMapView!
+    
+    // Variables -:
+    var locationManager = CLLocationManager()
+    let authorizationStatus = CLLocationManager.authorizationStatus() // to keep track if we let the app using our location or not
+    let regionRadius : Double = 1000
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        mapView.delegate = self
+        locationManager.delegate = self
+        configureLocationServices()
     }
 
+    // Actions -:
+    @IBAction func centerCurrLocBtnPressed(_ sender: Any) {
+        if authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse {
+            centerMapOnUserLocation()
+        }
+    }
+    
+}
 
+extension MapVC : MKMapViewDelegate {
+    func centerMapOnUserLocation(){
+        guard let coordinate = locationManager.location?.coordinate else { return } // This will bring us the location of the blue point
+        let coordinatRegion = MKCoordinateRegion(center: coordinate, latitudinalMeters: regionRadius * 2.0, longitudinalMeters: regionRadius * 2.0 )
+        mapView.setRegion(coordinatRegion, animated: true)
+        
+    }
+    
+}
+
+extension MapVC : CLLocationManagerDelegate {
+    
+    func configureLocationServices(){
+        if authorizationStatus == .notDetermined { // if out authroization not determined then we should ask the user to give us a permission
+            locationManager.requestAlwaysAuthorization()
+        }else { // otherwise we don't need to ask the user again for permision
+            return
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        centerMapOnUserLocation()
+    }
+    
 }
 
